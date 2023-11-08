a6f3d880262a7806d95465dc07e573e8ce49288a
quin <queenshafaraniabilqis@gmail.com>
2023-11-08 20:48:54 +0700

    js belum selesai om

---
+++ index.js
@@ -0,0 +1,18 @@
+let container = document.querySelector(".container");
+let gridButton = document.querySelector("submit-grid");
+let clearGridButton = document.querySelector(".clear-grid");
+let gridWidth = document.querySelector("width-range");
+let gridHeight = document.querySelector("height-range");
+let colorButton = document.querySelector("color-input");
+let eraseBtn = document.querySelector("erase-btn");
+let paintBtn = document.querySelector("paint-btn");
+let widthValue = document.querySelector("width-value");
+let heightValue = document.querySelector("height-value");
+
+let events = {
+  mouse: {
+    down: "mousedown",
+    move: "mousemove",
+    up: "mouseup",
+  },
+};
